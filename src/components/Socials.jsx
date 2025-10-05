b4221181249a61a3c2cf487ad2264faa1aeff8ab
Architrb1795 <archit1317@gmail.com>
2025-10-05 14:26:32 +0530

    Updated Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -21,7 +21,7 @@ const Socials = () => {
             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
-          <span className="instagram-text">@PromNight2025</span>
+          <span className="instagram-text">@iitroorkeefresher2025</span>
         </a>
       </div>
     </section>
